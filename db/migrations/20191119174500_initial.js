1f3d1bbbefcc778dfe843bc63bf5095958de9e45
posi7790 <posi7790@colorado.edu>
2019-11-19 19:47:16 -0700

    Create intial schema with migrations

--- db/migrations/20191119174500_initial.js
+++ db/migrations/20191119174500_initial.js
@@ -1,8 +1,41 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return Promise.all([
+    knex.schema.createTable('talks', function (table) {
+      table.increments('id').primary();
+      table.string('headline');
+      table.string('speaker1_id')
+        .unsigned()
+        .references('speakers.id');
+      table.string('speaker2_id')
+        .unsigned()
+        .references('speakers.id')
+      table.string('speaker3_id')
+        .unsigned()
+        .references('speakers.id');
+      table.string('speaker4_id')
+        .unsigned()
+        .references('speakers.id');
+      table.date('publish_date')
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('speakers', function (table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('occupation');
+      table.string('introduction');
+
+      table.timestamps(true, true);
+    })
+  ])
 };
 
-exports.down = function(knex) {
-  
+
+exports.down = function (knex) {
+  return Promise.all([
+    knex.schema.dropTable('talks'),
+    knex.schema.dropTable('speakers')
+  ]);
 };

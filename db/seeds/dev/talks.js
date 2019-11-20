4a81ad641ee62e2288fbe5307f75f9738e07247c
posi7790 <posi7790@colorado.edu>
2019-11-20 15:18:48 -0700

    Add all talk data to database that is not using foreign id

--- db/seeds/dev/talks.js
+++ db/seeds/dev/talks.js
@@ -1,14 +1,46 @@
-var json = require('../../../data/talkData.json');
+var talksData = require('../../../data/talkData.json');
 
 exports.seed = function (knex) {
 
   return knex('speakers').del()
     .then(() => knex('talks').del())
     .then(() => {
-      return Promise.all([
+      let talkPromises = [];
 
-      ]);
+      talksData.forEach(talk => {
+        talkPromises.push(createTalk(knex, talk));
+      });
+
+      return Promise.all(talkPromises);
     })
-    .then(talks => console.log(talks))
     .catch(error => console.log(`Error seeding data: ${error}`));
 };
+
+const createTalk = (knex, talk) => {
+  return knex('talks').insert({
+    headline: talk.headline,
+    description: talk.description,
+    views: talk.views,
+    published: talk.published,
+  }, 'id')
+    .then(speakerId => {
+      let speakerPromises = [];
+      console.log(speakerId)
+      talk.speakers.forEach(speaker => {
+        speakerPromises.push(
+          createSpeaker(knex, {
+            name: speaker.name,
+            occupation: speaker.occupation,
+            introduction: speaker.introduction,
+            id: speakerId[0]
+          })
+        )
+      });
+
+      return Promise.all(speakerPromises);
+    });
+};
+
+const createSpeaker = (knex, speaker) => {
+  return knex('speakers').insert(speaker);
+}

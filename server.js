20e01ad2e268815e82dc3d58623fa954c19e973e
posi7790 <posi7790@colorado.edu>
2019-11-19 18:07:44 -0700

    Add GET request for all talks and individual talks based on id

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ app.get('/', (request, response) => {
   response.send('Welcome to TEDTalks Data');
 });
 
+
+app.get('/api/v1/talks', (request, response) => {
+  const { talks } = app.locals;
+
+  response.json({ talks });
+});
+
+app.get('/api/v1/talks/:id', (request, response) => {
+  const { id } = request.params;
+  const talk = app.locals.talks.find(talk => talk.Talk_ID === id);
+  if (!talk) {
+    return response.sendStatus(404);
+  }
+
+  response.status(200).json(talk);
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });

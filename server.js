9b2831cdcc98ea9ff3a08116f66fc150154e4a47
posi7790 <posi7790@colorado.edu>
2019-11-19 19:57:38 -0700

    Configure to work with the knex database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 const express = require('express');
 const shortid = require('shortid');
 const app = express();
